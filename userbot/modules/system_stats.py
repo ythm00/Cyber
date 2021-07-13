6724318f7255de245e15d041063e47ccaaaf0c3a
Irham <86714351+ythm00@users.noreply.github.com>
2021-07-14 06:15:52 +0700

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -5,6 +5,8 @@
 #
 """ Userbot module for getting information about the server. """
 
+import time
+
 from asyncio import create_subprocess_exec as asyncrunapp
 from asyncio.subprocess import PIPE as asyncPIPE
 from os import remove
