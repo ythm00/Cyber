a9133b5b926e72f509ab4a165a337e284e4188c7
ythm00 <86714351+ythm00@users.noreply.github.com>
2021-07-14 04:21:11 +0700

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -15,7 +15,7 @@ from git import Repo
 from telethon import version
 from telethon.errors.rpcerrorlist import MediaEmptyError
 
-from userbot import ALIVE_LOGO, ALIVE_NAME, CMD_HELP, bot, StarTime
+from userbot import ALIVE_LOGO, ALIVE_NAME, CMD_HELP, bot, StartTime
 
 from userbot.events import register
 
