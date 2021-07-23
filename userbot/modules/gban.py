d1a281dd23d4cefa55761ead7738a624c93f9f05
Irham <86714351+ythm00@users.noreply.github.com>
2021-07-23 12:19:39 +0700

    Update gban.py

--- userbot/modules/gban.py
+++ userbot/modules/gban.py
@@ -118,7 +118,8 @@ async def global_ban(event):
     if not user:
         return
     if user.id == (await event.client.get_me()).id:
-        return await event.edit("`Why would you gban yourself?`")
+       await event.edit("`Why would you gban yourself?`")
+        return
     if user.id in DEVELOPER:
         await event.edit("He is mya maker.")
         return
