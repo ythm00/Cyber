069629f6d3fc5e1802139fbdac08c12ef4da04ee
IrhamFadzillah <irham8381@gmail.com>
2021-08-04 23:47:37 +0000

    pylint: auto fixes
    Signed-off-by: IrhamFadzillah <irham8381@gmail.com>

--- userbot/modules/__help.py
+++ userbot/modules/__help.py
@@ -2,10 +2,12 @@ from telethon.errors.rpcerrorlist import BotInlineDisabledError as dis
 from telethon.errors.rpcerrorlist import BotMethodInvalidError
 from telethon.errors.rpcerrorlist import BotResponseTimeoutError as rep
 
-from ..core import *
-from ..utils import edit_or_reply
 from userbot import CMD_HELP
 from userbot.event import register
+
+from ..core import *
+from ..utils import edit_or_reply
+
 
 @register(outgoing=True, pattern=r"^\.helpme")
 async def _help(cyb):
